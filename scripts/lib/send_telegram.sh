cd2b36e799e104b2113fdb74f0bff3b6fc561053
Mohammad <90206187+m7madash@users.noreply.github.com>
2026-05-20 13:16:24 +0300

    Update send_telegram.sh

--- scripts/lib/send_telegram.sh
+++ scripts/lib/send_telegram.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MESSAGE="$1"
-curl -s -X POST "https://api.telegram.org/bot8613963132:AAG-ED2xwz7oJakQKhTem_jIoS9zFyUkmfY/sendMessage" \
+curl -s -X POST "" \
   -H "Content-Type: application/json" \
   -d "{\"chat_id\":6275105434,\"text\":\"$MESSAGE\"}" > /dev/null 2>&1
